6312e6abbefde1403f7968f058d7aa748dc544fa
root <root@ip-172-31-84-97.ec2.internal>
2021-12-25 17:00:36 +0000

    Print the uname and passwd

--- samplefile.sh
+++ samplefile.sh
@@ -3,3 +3,5 @@ echo This is the test to creat user using the script
 read -p "Please Enter the username: " Username
 #read -s -p "Please enter the password: " Password
 Password=$(curl https://random.justyy.workers.dev/api/random/?cached&n=8)
+echo $Username
+echo $password
